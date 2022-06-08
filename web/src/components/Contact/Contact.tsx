e2152fb42395e8aaee625cd527b59f44c56efd58
Sai Sridhar Akula <sridhar.akula001@gmail.com>
2022-06-08 19:23:26 +0530

    Mutation and Validation

--- web/src/components/Contact/Contact.tsx
+++ web/src/components/Contact/Contact.tsx
@@ -6,7 +6,24 @@ import {
   SubmitHandler,
   FieldError,
   Label,
+  FormError,
+  useForm,
 } from '@redwoodjs/forms'
+import { useMutation } from '@redwoodjs/web'
+import { toast, Toaster } from '@redwoodjs/web/toast'
+
+import {
+  CreateContactMutation,
+  CreateContactMutationVariables,
+} from 'types/graphql'
+
+const CREATE_CONTACT = gql`
+  mutation CreateContactMutation($input: CreateContactInput!) {
+    createContact(input: $input) {
+      id
+    }
+  }
+`
 
 interface FormValues {
   name: string
@@ -15,50 +32,71 @@ interface FormValues {
 }
 
 const Contact = () => {
+  const [create, { loading, error }] = useMutation<
+    CreateContactMutation,
+    CreateContactMutationVariables
+  >(CREATE_CONTACT, {
+    onCompleted: () => {
+      toast.success('Thank you for your submission!')
+      formMethods.reset()
+    },
+  })
+
+  const formMethods = useForm({ mode: 'onBlur' })
+
   const onSubmit: SubmitHandler<FormValues> = (data) => {
-    console.log(data)
+    create({ variables: { input: data } })
   }
 
   return (
-    <Form onSubmit={onSubmit} config={{ mode: 'onBlur' }}>
-      <Label name="name" errorClassName="error">
-        Name
-      </Label>
-      <TextField
-        name="name"
-        validation={{ required: true }}
-        errorClassName="error"
-      />
-      <FieldError name="name" className="error" />
+    <>
+      <Toaster />
+      <Form
+        onSubmit={onSubmit}
+        config={{ mode: 'onBlur' }}
+        error={error}
+        formMethods={formMethods}
+      >
+        <FormError error={error} wrapperClassName="form-error" />
+        <Label name="name" errorClassName="error">
+          Name
+        </Label>
+        <TextField
+          name="name"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="name" className="error" />
 
-      <Label name="email" errorClassName="error">
-        Email
-      </Label>
-      <TextField
-        name="email"
-        validation={{
-          required: true,
-          pattern: {
-            value: /^[^@]+@[^.]+\..+$/,
-            message: 'Please enter a valid email address',
-          },
-        }}
-        errorClassName="error"
-      />
-      <FieldError name="email" className="error" />
+        <Label name="email" errorClassName="error">
+          Email
+        </Label>
+        <TextField
+          name="email"
+          validation={{
+            required: true,
+            pattern: {
+              value: /^[^@]+@[^.]+\..+$/,
+              message: 'Please enter a valid email address',
+            },
+          }}
+          errorClassName="error"
+        />
+        <FieldError name="email" className="error" />
 
-      <Label name="message" errorClassName="error">
-        Message
-      </Label>
-      <TextAreaField
-        name="message"
-        validation={{ required: true }}
-        errorClassName="error"
-      />
-      <FieldError name="message" className="error" />
+        <Label name="message" errorClassName="error">
+          Message
+        </Label>
+        <TextAreaField
+          name="message"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="message" className="error" />
 
-      <Submit>Save</Submit>
-    </Form>
+        <Submit disabled={loading}>Save</Submit>
+      </Form>
+    </>
   )
 }
 
