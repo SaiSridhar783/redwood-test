580052b8f090ad55464d93b552c8baa625b42013
Sai Sridhar Akula <sridhar.akula001@gmail.com>
2022-06-08 17:01:09 +0530

    Cell,Scaffold,Page,Layout

---
+++ web/src/components/ArticlesCell/ArticlesCell.tsx
@@ -0,0 +1,52 @@
+import type { ArticlesQuery } from 'types/graphql'
+import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
+import { Link, routes } from '@redwoodjs/router'
+
+export const QUERY = gql`
+  query ArticlesQuery {
+    articles: posts {
+      id
+      title
+      body
+      createdAt
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }: CellFailureProps) => (
+  <div style={{ color: 'red' }}>Error: {error.message}</div>
+)
+
+export const Success = ({ articles }: CellSuccessProps<ArticlesQuery>) => {
+  return (
+    <>
+      {articles.map((article) => (
+        <article key={article.id}>
+          <header>
+            <h2>
+              <Link to={routes.article({ id: article.id })}>
+                {article.title}
+              </Link>
+            </h2>
+          </header>
+          <p>{article.body}</p>
+          <div>
+            Posted at:&nbsp;
+            {new Date(article.createdAt).toLocaleDateString('en-IN', {
+              year: 'numeric',
+              month: 'long',
+              day: 'numeric',
+              hour: 'numeric',
+              hour12: true,
+              minute: 'numeric',
+            })}
+          </div>
+        </article>
+      ))}
+    </>
+  )
+}
