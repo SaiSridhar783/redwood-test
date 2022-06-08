1a2ac8147e5b9354d9584733b37e03ae25f30344
Sai Sridhar Akula <sridhar.akula001@gmail.com>
2022-06-08 17:12:07 +0530

    Article Componenet

--- web/src/components/ArticlesCell/ArticlesCell.tsx
+++ web/src/components/ArticlesCell/ArticlesCell.tsx
@@ -1,6 +1,6 @@
 import type { ArticlesQuery } from 'types/graphql'
 import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
-import { Link, routes } from '@redwoodjs/router'
+import Article from 'src/components/Article'
 
 export const QUERY = gql`
   query ArticlesQuery {
@@ -25,27 +25,7 @@ export const Success = ({ articles }: CellSuccessProps<ArticlesQuery>) => {
   return (
     <>
       {articles.map((article) => (
-        <article key={article.id}>
-          <header>
-            <h2>
-              <Link to={routes.article({ id: article.id })}>
-                {article.title}
-              </Link>
-            </h2>
-          </header>
-          <p>{article.body}</p>
-          <div>
-            Posted at:&nbsp;
-            {new Date(article.createdAt).toLocaleDateString('en-IN', {
-              year: 'numeric',
-              month: 'long',
-              day: 'numeric',
-              hour: 'numeric',
-              hour12: true,
-              minute: 'numeric',
-            })}
-          </div>
-        </article>
+        <Article key={article.id} article={article} />
       ))}
     </>
   )
